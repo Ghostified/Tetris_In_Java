c7170ed1b86cb5f12f321724677aa68a1c34207c
Allan Branson <bransonaallan@gmail.com>
2024-05-22 23:44:37 +0300

    Create a game loop using abthread ,

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,9 +1,10 @@
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
 
 import javax.swing.JPanel;
 
-public class GamePanel  extends JPanel{
+public class GamePanel  extends JPanel implements Runnable{
 
     public static final int WIDTH = 1280;
     public static final int HEIGHT = 720;
@@ -18,10 +19,48 @@ public class GamePanel  extends JPanel{
         this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
         this.setBackground(Color.BLACK);
         this.setLayout(null);
+    }
+
+    //loop to run the game
+    public void launchGame() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
 
 
-        //Create a game loop to update the screen at regular intervals
-        //eg seting fps to 60
+
+    //Create a run method to run the game loop
+    @Override
+    public void run () {
+        //Game Loop that runs the game at a specific FPS
+        double drawInterval = 1000000000 / FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+
+        while (gameThread != null) {
+            currentTime = System.nanoTime();
+
+            delta += (currentTime - lastTime) / drawInterval;
+            lastTime = currentTime;
+            if (delta >= 1) {
+                update();
+                repaint();
+                delta--;
+            }
+        }
+        
+    }
+
+    //Method to update the game
+    private void update() {
+        //Update objects in the game, i.e number information
+        
+    }
+
+    public void paintComponent (Graphics g) {
+        super.paintComponent(g);
+        //Draw the game - update ui text 
     }
 
 }
