30bb103b5c6f0cbe4e560a1c7915995279ad317f
Allan Branson <bransonaallan@gmail.com>
2024-05-25 21:27:21 +0300

    initial commit for main

---
+++ src/GamePanel.java
@@ -0,0 +1,77 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JPanel;
+
+public class GamePanel  extends JPanel implements Runnable{
+
+    public static final int WIDTH = 1280;
+    public static final int HEIGHT = 720;
+    final int FPS = 60; //refresh rate of the screen per second
+    Thread gameThread; // thread to run the game loop
+
+    //Instantiate the PlayManager class
+    PlayManager playManager = new PlayManager();
+
+
+
+    public GamePanel() {
+
+        //Panel Settings
+        this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        this.setBackground(Color.BLACK);
+        this.setLayout(null);
+
+        playManager = new PlayManager();
+    }
+
+    //loop to run the game
+    public void launchGame() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+
+
+    //Create a run method to run the game loop
+    @Override
+    public void run () {
+        //Game Loop that runs the game at a specific FPS
+        double drawInterval = 1000000000 / FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+
+        while (gameThread != null) {
+            currentTime = System.nanoTime();
+
+            delta += (currentTime - lastTime) / drawInterval;
+            lastTime = currentTime;
+            if (delta >= 1) {
+                update();
+                repaint();
+                delta--;
+            }
+        }
+        
+    }
+
+    //Method to update the game
+    private void update() {
+        //call playManager update method
+        playManager.update();
+        
+    }
+
+    public void paintComponent (Graphics g) {
+        //parsre the graphics object to the playManager draw method
+        super.paintComponent(g);
+
+        //call the draw method
+        Graphics2D g2 = (Graphics2D)g;
+        playManager.draw(g2);
+    }
+
+}
---
+++ src/MainApp.java
@@ -0,0 +1,19 @@
+import javax.swing.JFrame;
+
+public class MainApp {
+    public static void main(String[] args) throws Exception {
+        
+        JFrame window = new JFrame("Simple Tetris");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+
+        //Add and set the Game Panel to the window/ jFrame
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+    }
+}
