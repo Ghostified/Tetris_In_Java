cc877cc04e7d91ce0c42edf9cb74cde9dbf9a335
Allan Branson <bransonaallan@gmail.com>
2024-05-28 02:43:17 +0300

    use the up key handler to switch the block direction

--- src/mino/Mino.java
+++ src/mino/Mino.java
@@ -12,6 +12,8 @@ public class Mino {
     public Block b [] = new Block[4];
     public Block tempB [] = new Block[4];
     int autoDropCounter = 0;
+    public int direction  = 1;  //There are four directions for each mino (1,2,3,4)
+
 
     public void create (Color c ) {
         b[0] = new Block(c);
@@ -25,7 +27,26 @@ public class Mino {
     }
 
     public void setXY (int x, int y) {}
-    public void updateXY (int direction) {}
+    public void updateXY (int direction) {
+
+        this.direction = direction;
+        b[0].x = tempB[0].x;
+        b[0].y = tempB[0].y;
+        b[1].x = tempB[1].x;
+        b[1].y = tempB[1].y;
+        b[2].x = tempB[2].x;
+        b[2].y = tempB[2].y;
+        b[3].x = tempB[3].x;
+        b[3].y = tempB[3].y;
+    }
+
+    //These methods are used to get the direction of the mino
+    public void getDirection1 () {}
+    public void getDirection2 () {}
+    public void getDirection3 () {}
+    public void getDirection4 () {}
+
+
     public void update () {
 
         //move the mino left, right, down or up based on the key pressed
@@ -60,9 +81,15 @@ public class Mino {
         }
         if (KeyHandler.upPressesd) {
 
+            //handles minos direction change
+            switch ( direction) {
+                case 1 : getDirection2();  break;
+                case 2 : getDirection3(); break ;
+                case 3 : getDirection4(); break;
+                case 4 : getDirection1(); break;
+            } 
+            KeyHandler.upPressesd = false ;
         }
-
-
         //drop the mino every 60 frames
         autoDropCounter++;
         if (autoDropCounter == PlayManager.dropInterval) { // Use the comparison operator '==' instead of '='
